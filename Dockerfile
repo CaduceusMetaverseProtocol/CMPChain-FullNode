6d3f6394a315d316465c313811ed6b7091ae8b2e
w <w123456@gmail.com>
2022-06-06 18:53:02 +0800

    add  config file for main net

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM cmp-chain-full-node:1.0.0
+
+# Install ca-certificates
+RUN apk add --no-cache --update ca-certificates py3-setuptools supervisor wget lz4 gzip
+
+# Temp directory for copying binaries
+RUN mkdir -p /tmp/bin
+WORKDIR /tmp/bin
+
+
+#WORKDIR $GAIAD_HOME
+
+# Expose ports
+EXPOSE 26656 26657 26658
+EXPOSE 8545
+
+# Add entrypoint script
+COPY ./scripts/entrypoint.sh /usr/local/bin/entrypoint.sh
+COPY ./fullnode /tmp
+RUN chmod u+x /usr/local/bin/entrypoint.sh
+ENTRYPOINT [ "/usr/local/bin/entrypoint.sh" ]
+
+STOPSIGNAL SIGINT
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+# exit script on any error
+set -e
+
+
+initialize() {
+	NODE_DIR=$1
+	BINARY=$2
+
+	if [ $# != 2 ]; then
+		echo "expected 4 arguments for initialize"
+		exit 1
+	fi
+
+	if [ ! -f "$NODE_DIR/config/genesis.json" ]; then
+		echo "no existing genesis file found, initializing.."
+		$BINARY init "${MONIKER:-nonamenode}" --home="$NODE_DIR" --chain-id="${CHAIN_ID:-256256}"
+		chmod 777 $NODE_DIR
+		cd "${NODE_DIR}/config"
+		rm -rf app.toml config.toml genesis.json 
+
+		cp -r /tmp/prod/config/*  ./
+		cp -r /tmp/prod/clicfg/ ../
+	fi
+}
+
+
+initialize "/cmp/node$ID" cmpd
+cd "/cmp/node$ID"
+exec supervisord --nodaemon --configuration /etc/supervisor/supervisord.conf
